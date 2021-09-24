8f1e2cd74f6cc24c7c8efdd7b62395adbe88b8f8
Sunny Bhawsar <sunnybhawsar07@gmail.com>
2021-09-25 01:16:20 +0530

    Addmin .Net Core 3.1 Web App

---
+++ AzWebApplication/AzWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
