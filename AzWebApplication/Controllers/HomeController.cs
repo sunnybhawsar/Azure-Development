4f7671fbc56de974b311018dc163c2d27a1934f8
Sunny Bhawsar <sunnybhawsar07@gmail.com>
2021-10-18 00:02:01 +0530

    Changes to read App Service appSettings

--- AzWebApplication/Controllers/HomeController.cs
+++ AzWebApplication/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using AzWebApplication.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,14 +13,18 @@ namespace AzWebApplication.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            ViewBag.Role = _configuration["role"];
+            ViewBag.SecretRole = _configuration.GetSection("secretRole").Value;
             return View();
         }
 
